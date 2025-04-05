876b1a1d4765256cfae400bc084dc35d773f5976
moesha463 <smirnovzena463@gmail.com>
2025-04-05 12:56:53 +0300

    feat: add collision detection and game over

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ framerate = 60;
 black = (0, 0, 0);
 white = (255, 255, 255);
 
+game_over = False;
+
+font = pygame.font.Font('freesansbold.ttf', 20);
+
 player_y = 130;
 computer_y = 130;
 ball_x = 145;
@@ -36,6 +40,14 @@ def update_ai(ball_y, computer_y):
     
     return computer_y;
 
+def check_collisions(ball, player, computer, ball_x_direction):
+    if ball.colliderect(player):
+        ball_x_direction = 1;
+    elif ball.colliderect(computer):
+        ball_x_direction = -1;
+    
+    return ball_x_direction;
+
 def update_ball(ball_x_direction, ball_y_direction, ball_x, ball_y, ball_speed, ball_y_speed):
     if ball_x_direction == 1 and ball_x < 290:
         ball_x += ball_speed;
@@ -59,6 +71,12 @@ def update_ball(ball_x_direction, ball_y_direction, ball_x, ball_y, ball_speed, 
 
     return ball_x_direction, ball_y_direction, ball_x, ball_y;
 
+def check_game_over(ball_x, game_over):
+    if ball_x <= 0 or ball_x >= 290 and game_over == False:
+        game_over = True;
+    
+    return game_over;
+
 while running:
     timer.tick(framerate);
     screen.fill(black);
@@ -66,12 +84,18 @@ while running:
     player = pygame.draw.rect(screen, white, [5, player_y, 10, 40]);
     computer = pygame.draw.rect(screen, white, [285, computer_y, 10, 40]);
     ball = pygame.draw.rect(screen, white, [ball_x, ball_y, 10, 10])
-    computer_y = update_ai(ball_y, computer_y);
 
-    ball_x_direction, ball_y_direction, ball_x, ball_y = update_ball(ball_x_direction, ball_y_direction, ball_x, ball_y, ball_speed, ball_y_speed)
-    
-    update_ball(ball_x_direction, ball_y_direction, ball_x, ball_y, ball_speed, ball_y_speed);
+    game_over = check_game_over(ball_x, game_over);
 
+    if not game_over:
+        computer_y = update_ai(ball_y, computer_y);
+        ball_x_direction, ball_y_direction, ball_x, ball_y = update_ball(ball_x_direction, ball_y_direction, ball_x, ball_y, ball_speed, ball_y_speed);
+
+    ball_x_direction = check_collisions(ball, player, computer, ball_x_direction);
+
+    if game_over:
+        game_over_text = font.render('Game Over', True, white, black);
+        screen.blit(game_over_text, (90, 130));
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
